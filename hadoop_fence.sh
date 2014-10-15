c1ff689ada17483cc579efeac586c9fed47c7620
shuai.lou <louis.hust@gmail.com>
2014-10-15 11:30:55 +0800

    hadoop fence is a script which used by dfs.ha.fencing.methods ha_dis_fen.sh, and this script is deployed aon the zookeeper machines.

---
+++ hadoop_fence.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+usage(){
+    echo "Usage: $0 hostname port"
+    echo "Note: hostname can not be ip"
+    exit 1
+}
+
+function valid_ip()
+{
+    local  ip=$1
+    local  stat=1
+
+    if [[ $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
+        OIFS=$IFS
+        IFS='.'
+        ip=($ip)
+        IFS=$OIFS
+        [[ ${ip[0]} -le 255 && ${ip[1]} -le 255 \
+            && ${ip[2]} -le 255 && ${ip[3]} -le 255 ]]
+        stat=$?
+    fi
+    return $stat
+}
+
+# invoke  usage
+# call usage() function if filename not supplied
+[[ $# -ne 2 ]] && usage
+
+host=$1
+port=$2
+
+# check hostname
+if valid_ip $1; then
+    usage
+fi
+
+# 1. ping the host
+ping -c 1 $host > /dev/null
+if [ $? -eq 1 ]; then
+    echo "Host can not be reached!"
+    exit 0
+fi
+
+# 2. try to kill the process
+ssh louis@$host "fuser -v -k -n tcp $port"
+
+if [ $? -eq 0 ]; then
+    echo "Kill the active namenode successfully!"
+    exit 0
+else
+# 3. check the port works
+    ssh louis@$host "nc -z $host $port"
+    if [ $? -eq 0 ]; then
+        echo "Unable to fence - it is running but we cannot kill it!"
+        exit 1
+    else
+        echo "Active namenode process have been killed!"
+        exit 0
+    fi
+fi
